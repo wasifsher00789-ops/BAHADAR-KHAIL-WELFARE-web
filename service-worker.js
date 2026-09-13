be5e1eb3e6d4dafe00a1afcf1f8f805c2c65b60f
wasifsher00789-ops <wasifsher00789@gmail.com>
2026-09-13 14:43:34 +0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,4 @@
+const CACHE_NAME='bk-welfare-v2';
+self.addEventListener('install',e=>{self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim());});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));});
